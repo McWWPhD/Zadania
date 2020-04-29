b0e2770b1525ab706f8b203ff4323c72559d95cf
wojciech w <mcwwphd@gmail.com>
2020-04-29 21:09:29 +0200

    Dodano Zadanie 12

---
+++ Liczby/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liczby
+{
+
+    /*Takie proste zadanie związane z liczbami :)
+Przyjmujemy od użytkownika ciąg liczb całkowitych oddzielając je przecinkiem
+Parsujemy taki ciąg znaków do tablicy/listy - wybór dowolny
+Z tej tablicy/listy wartości liczymy takie rzeczy jak: wartość minimalna, wartość maks. , średnia arytmetyczna, mediana, odchylenie standardowe*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
